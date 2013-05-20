997b689d1460700556a19c11a6faacfbecdee0e7
pianowow <pianowow@gmail.com>
2013-05-19 23:48:24 -0400

    debug build

--- arena.py
+++ arena.py
@@ -9,6 +9,10 @@
 #TODO
 
 #simulate from the middle of the game, have score be an input (need a way to force the engine to choose different words first though)
+import os
+
+print(os.getcwd())
+
 
 from player import player0, player1
 from time import time
